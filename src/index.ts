27917f25ecafd1c4cf288db63e6eaa83c8cd4151
Sune Simonsen <sune@we-knowhow.dk>
2025-06-09 15:44:50 +0200

    Default context field on components

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ type VoidProps = {
   children?: void;
 };
 
+type Context = Record<string, any>;
+
 export abstract class Component<TProps = VoidProps> {
   props: TProps | undefined;
+  context: Context = {};
   abstract render(props: TProps): JSX.Nodes;
 }
